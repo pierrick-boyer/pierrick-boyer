86e94695c24450fa48412955e4ecbb41193944a1
pierrick <pierrick.boyer43@gmail.com>
2022-03-21 18:12:03 +0100

    Python function

---
+++ Python/Django/Reset_password/reset_password.py
@@ -0,0 +1,27 @@
+""" Simple function to reset user password using basic Django forms. This example does not include the front-end. """
+
+from django.shortcuts import render, redirect
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+
+
+@login_required
+def change_password(request):
+
+    if request.method == 'POST':
+        password_form = PasswordChangeForm(request.user, request.POST)
+
+        if password_form.is_valid():
+            user = password_form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Password was successfully updated', extra_tags='success')
+            return redirect('profile')
+
+    else:
+        password_form = PasswordChangeForm(request.user)
+
+    return render(request, 'app9/change_password.html', {'password_form': password_form})

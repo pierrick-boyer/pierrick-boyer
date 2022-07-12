bc91ec889cf484d96780462d45ad77c61c5391f8
pierrick-boyer <pierrick.boyer43@gmail.com>
2022-07-12 13:08:35 +0200

    first module exercise to understand how it works

---
+++ Python/exercices/first_module/progs/main.py
@@ -0,0 +1,15 @@
+# import sys
+
+from sys import path
+from Python.exercices.first_module.modules import module
+
+
+# for p in sys.path:
+#     print(p)
+
+path.append('..\\modules')
+
+zeroes = [0 for i in range(5)]
+ones = [1 for j in range(5)]
+print(module.suml(zeroes))
+print(module.prodl(ones))

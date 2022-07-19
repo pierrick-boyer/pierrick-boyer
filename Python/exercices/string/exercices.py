bfe819c3c6c7289661979ec681a5c7c462ff38dd
pierrick-boyer <pierrick.boyer43@gmail.com>
2022-07-19 11:53:45 +0200

    string exercise

--- Python/exercices/string/exercices.py
+++ Python/exercices/string/exercices.py
@@ -1,3 +1,23 @@
 str_1 = input("Enter the first string: ")
 # remove space and letters to upper case
 print(''.join(sorted(list(str_1.upper().replace(' ', '')))))
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def search():
+    word = input("Enter the word you wish to find: ").upper()
+    strn = input("Enter the string you wish to search through: ").upper()
+
+    start = 0
+
+    if word.find(strn):
+        start += strn.count(word)
+        return start
+    else:
+        return False
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
